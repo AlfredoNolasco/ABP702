8043d90f16c9b778bdbc7eba5d0e667d07cf7b0c
Mario Bernal <Mario Bernal>
2016-11-09 23:50:52 -0600

    iconos

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,6 @@
   <footer> 
-        <div class="footer-copyright blue">
-          <div class="container center "> 
+        <div class="footer-copyright indigo">
+          <div class="container center white-text">
               &copy 2016 Aeroline702
               | Acerca del Sitio | Avisos Legales | Mapa de Sitio | Contáctanos |
           </div>
--- views/partials/head.php
+++ views/partials/head.php
@@ -16,18 +16,25 @@
       </script>
     </head>
       <nav>
-        <div class="nav-wrapper blue">
+        <div class="nav-wrapper indigo">
         <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
           <a href="#!" class="brand-logo center">AEROLINE702</a>
           <ul class="right hide-on-med-and-down">
-            <li><a href="../../">Salir</a></li>
+            <li><a href="../../" class="waves-effect waves-light hover_salir"><i class="material-icons left">input</i>Salir</a></li>
           </ul>
           <ul class="left hide-on-med-and-down">
-            <li class="active"><a href="#">Inicio</a></li>
+            <li><a href="../personal"><i class="material-icons left">supervisor_account</i>Personal</a></li>
+            <li><a href="../Destinos"><i class="material-icons left">location_on</i>Destinos</a></li>
+            <li><a href="../aviones_mant"><i class="material-icons left">navigation</i>Aviones</a></li>
+            <li><a href="../analisis_financiero"><i class="material-icons left">library_books</i>Analisis</a></li>
           </ul>
           <ul  id="slide-out" class="side-nav">
-            <li class="active"><a href="#">Inicio</a></li>
-            <li><a href="#">Salir</a></li>     
+            <li class="active"><a href="#"><i class="material-icons left">home</i>Inicio</a></li>
+            <li><a href="../personal"><i class="material-icons left">supervisor_account</i>Personal</a></li>
+            <li><a href="../Destinos"><i class="material-icons left">location_on</i>Destinos</a></li>
+            <li><a href="../aviones"><i class="material-icons left">navigation</i>Aviones</a></li>
+            <li><a href="../analisis_financiero"><i class="material-icons left">library_books</i>Analisis</a></li>
+            <li><a href="../../"><i class="material-icons left">input</i>Salir</a></li>     
           </ul>
         </div>
       </nav>
