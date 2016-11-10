eb55542ba8edd61fc82d3f716e365e1b73a2bca7
lalooo <laloo0153@gmail.com>
2016-11-10 16:25:37 -0600

    head del admin

---
+++ views/partials/head_admon.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <title>Aeroline702</title>
+      <link type="text/css" rel="stylesheet" href="../../resources/css/materialize.min.css"/>
+      <link type="text/css" rel="stylesheet" href="../../resources/css/estilos.css">
+      <script type="text/javascript" src="../../resources/js/jquery-2.2.3.min.js"></script>
+      <script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
+      <script type="text/javascript" src="../../resources/js/jquery.validate.min.js"></script>
+      <script type="text/javascript">
+          $(document).ready(function(){
+             $(".button-collapse").sideNav();
+              
+          });
+      </script>
+    </head>
+      <nav>
+        <div class="nav-wrapper indigo">
+        <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+          <a href="#!" class="brand-logo center">AEROLINE702</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="../../" class="waves-effect waves-light hover_salir"><i class="material-icons left">input</i>Salir</a></li>
+          </ul>
+          <ul class="left hide-on-med-and-down">
+            <li><a href="../personal"><i class="material-icons left">supervisor_account</i>Personal</a></li>
+            <li><a href="../Destinos"><i class="material-icons left">location_on</i>Destinos</a></li>
+            <li><a href="../aviones_mant"><i class="material-icons left">navigation</i>Aviones</a></li>
+            <li><a href="../analisis_financiero"><i class="material-icons left">library_books</i>Analisis</a></li>
+          </ul>
+          <ul  id="slide-out" class="side-nav">
+            <li class="active"><a href="#"><i class="material-icons left">home</i>Inicio</a></li>
+            <li><a href="../personal"><i class="material-icons left">supervisor_account</i>Personal</a></li>
+            <li><a href="../Destinos"><i class="material-icons left">location_on</i>Destinos</a></li>
+            <li><a href="../aviones"><i class="material-icons left">navigation</i>Aviones</a></li>
+            <li><a href="../analisis_financiero"><i class="material-icons left">library_books</i>Analisis</a></li>
+            <li><a href="../../"><i class="material-icons left">input</i>Salir</a></li>     
+          </ul>
+        </div>
+      </nav>
+
+    <body>
