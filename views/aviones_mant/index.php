6d29fd2b77bf9948e6a004a9b2d95e050d10a2ec
delgadomk1 <delgado.mk1@gmail.com>
2016-11-10 21:14:27 -0600

    modificacion_aviones

--- views/aviones_mant/index.php
+++ views/aviones_mant/index.php
@@ -1,268 +1,212 @@
 <?php
   include("../partials/head.php")
 ?>
-<script type="text/javascript">
-	$(document).ready(function() 
-	{
-		$('select').material_select();
-        $("#agregar").click(function()
-        {
-            $("#mantenimiento").openModal();
-        });
-
-        $("#acepta_registro").click(function(event)
-        {
-            $("#form_mantenimiento").submit();
-        });
-
-        $("#form_mantenimiento").validate({
-        ignore:[],
-        rules:{
-          trabajador:"required",
-          pieza:"required",
-          descripcion:"required",
-          cantidad:"required",
-          costo:
-          {
-            required:true,
-            number:true
-          },
-        },
-        messages:{
-          trabajador:"Ingresa nombre del Mecanico",
-          pieza:"Ingresa el nombre de la pieza",
-          descripcion:"Ingresa una breve descripcion",
-          cantidad:"Selecciona cantidad de pieza",
-          costo:
-          {
-            required:"Ingresa el costo total de reparacion",
-            number:"Ingresa solamente numeros",
-          },
-        },
-
-        errorElement:'div',
-        errorPlacement:function(error,element){
-          var placement=$(element).data("error");
-          if(placement)
-            $(placement).append(error);
-          else
-            error.insertAfter(element);
-        },
-        submitHandler:function(form){
-          alert("todo ok");
-          $("#mantenimiento").closeModal();
-        }
-      });
-        // Validar el material ingresado
-        $("#agrmate").click(function()
-        {
-          $("#modmate").openModal();
-        });
-        $("#acepmaterial").click(function(event)
-        {
-          $("#form_material").submit();
-        });
-        $("#form_material").validate(
-        {
-          ignore:[],
-          rules:{
-          material:"required",
-          descrimater:"required",
-          cantidadmate:
-          {
-            required:true,
-            number:true
-          },
-          tipom:"required",
-          descritipom:"required",
-          costomate:
-          {
-            required:true,
-            number:true
-          },
-        },
-        messages:{
-          material:"Ingresa nombre del Mecanico",
-          descrimater:"Ingresa el nombre de la pieza",
-          cantidadmate:
-          {
-            required:"Ingresa una breve descripcion",
-            number:"Solamente Numeros"
-          },
-          tipom:"Selecciona cantidad de pieza",
-          descritipom:"Ingresa una breve descripcion del tipo de la pieza",
-          costomate:
-          {
-            required:"Ingresa el costo de la pieza",
-            number:"Ingresa solamente numeros"
-          },
-        },
-
-        errorElement:'div',
-        errorPlacement:function(error,element){
-          var placement=$(element).data("error");
-          if(placement)
-            $(placement).append(error);
-          else
-            error.insertAfter(element);
-        },
-        submitHandler:function(form){
-          // alert("todo ok");
-          // $("#mantenimiento").closeModal();
-        }
-        });
-	});
-</script>
 <body>
+<script type="text/javascript">
+  $(document).ready(function(event)
+  {
+        $('select').material_select();
+
+        $("#btn_agregar").click(function()
+        {
+            $("#test1");
+        });
+
+    $("#acepta_registro").click(function(event)
+    {
+      $("#form_aviones").submit();
+    });
 
 
-	<div class="row">
-		<div class="center">
-			<h3 class="center">Servicio de Mantenimiento</h3>
-             <a class=" btn-large waves-effect waves-light blue" id="agregar"><i class="material-icons">add</i>Nuevo Mantenimiento</a>
-             <a class=" btn-large waves-effect waves-light blue" id="agrmate"><i class="material-icons">settings</i>Material</a>
-		</div> 
-	</div>
+    $("#form_aviones").validate({
+        ignore:[],   
+        rules:{
+          nombre:"required",
+          descripcion:"required",
+          asiento:
+          {
+            required:true,
+            number:true
+          },
+          combustible:
+          {
+            required:true,
+            number:true
+          },
+          tipo:"required"
+        },
+        messages:{
+          nombre:"Ingresa el modelo del avion",
+          asiento:
+          {
+            required:"Ingresa numero de asientos",
+            number:"Solamente numeros"
+          },
+          descripcion:"Ingresa una breve descripcion",
+          combustible:
+          {
+            required:"Ingresa cantidad de combustible",
+            number:"Ingresa numeros"
+          },
+          tipo:"Ingresa tipo de vuelo"
+            
+        },
+        errorElement:'div',
+        errorPlacement:function(error,element)
+        {
+            var placement=$(element).data("error");
+            if(placement)
+              $(placement).append(error);
+            else
+              error.insertAfter(element);
+        },
+        submitHandler:function(form)
+        {
+            alert("todo ok");
+          $("#modal_agregar").closeModal();
+        }
+         
+    
+  });
+  });
+ </script>
 
+<main>
+     <div class="row col s12 white" style="width: 100%;">
+      <div class="col s12">
+            <ul class="tabs center">
+              <li class="tab col l3 m4 s12" id="id_aviones" class="id_aviones" ><a class="indigo-text" href="#test1" >AVIONES</a></li>
+              <li class="tab col l3 m4 s12"><a class="indigo-text" id="id_mantenimiento" class="id_mantenimiento" href="#test2">MANTENIMIENTO</a></li>
+              <li class="tab col l3 m4 s12"><a class="indigo-text" id="id_informe" class="id_informe" href="#test3">INFORME</a></li>
+            </ul>
+       </div>
+      </div> 
+      
+      <br>
+      <br>
 
-
-    <div class="modal" id="mantenimiento">
-        <div class="modal-content">
-        <h5 align="center">Mantenimiento</h5>
-        <br>
-            <div class="container row">
-                <form id="form_mantenimiento" name="form_mantenimiento">
-                <div class="input-field">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input id="trabajador" type="text" name="trabajador" data-error=".errortrabajador">
-                        <label for="trabajador">Nombre Del Mecanico</label><br>
-                        <div class="errortrabajador red-text"></div>
+<div id="test1">
+<div class="row">
+               <h5 align="left" class="col offset-l1">Aviones</h5>
+                <br>
+                <br>
+                <form id="form_aviones" name="form_aviones">
+                
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">airplanemode_active</i>
+                        <input id="nombre" type="text" name="nombre" class="validate" data-error=".errornombre">
+                        <label for="nombre">Nombre Del Avion</label><br>
+                        <div class="errornombre red-text"></div>
               
+                    </div>
+                 </div>
+                 
+                 <div class="row">
+                    <div class="input-field col s6 offset-l2">
+                        <i class="material-icons prefix">airline_seat_recline_extra</i>
+                        <input id="asiento" type="text" name="asiento" class="validate" data-error=".errorasiento">
+                        <label for="asiento">Numero de Asientos</label><br>
+                        <div class="errorasiento red-text"></div>
+              
+                    </div>
                 </div>
-                 <div class="input-field">
-                        <i class="material-icons prefix">settings</i>
-                        <input id="pieza" type="text" name="pieza" data-error=".errorpieza">
-                        <label for="pieza">Nombre De La Pieza</label><br>
-                        <div class="errorpieza red-text"></div>
-                </div>
-                <div class="input-field">
+                  
+                  <div class="row"> 
+                    <div class="input-field col s6 offset-l2">
                         <i class="material-icons prefix">assignment</i>
-                        <textarea id="descripcion" name="descripcion" class="validate" data-error=".errordescripcion" class="materialize-textarea" rows="10" placeholder="Breve Descripcion De La Pieza"></textarea>
-                        <label for="descripcion"><b>Descripcion De La Pieza</b></label>
+                        <textarea id="descripcion"  name="descripcion" class="validate" data-error=".errordescripcion" class="materialize-textarea" rows="10" placeholder="Breve Descripcion Del Avion"></textarea>
+                        <label for="descripcion"><b>Descripcion Del Avion</b></label>
                         <div class="errordescripcion red-text"></div>
               
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s6 offset-l2">
+                        <i class="material-icons prefix">local_gas_station</i>
+                        <input id="combustible" type="text" name="combustible" class="validate" data-error=".errorcombustible">
+                        <label for="combustible">Capacidad de tanque en litros</label><br>
+                        <div class="errorcombustible red-text"></div>
               
                     </div>
-                <div class="input-field">
-                        <i class="material-icons prefix">loupe</i>
-                        <select name="cantidad" id="cantidad" data-error=".errorcantidad">
-                                <option value="" disabled selected>Cantidad De Piezas</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="6">7</option>
-                                <option value="6">8</option>
-                                <option value="6">9</option>
-
+                   </div>
+                   
+                   <div class="row"> 
+                    <div class="input-field col s6 offset-l2">
+                        <i class="material-icons prefix">equalizer</i>
+                        <select name="tipo" id="tipo" class="validate" data-error=".errortipo">
+                                <option value="" disabled selected>Tipo de vuelo</option>
+                                <option value="1">Clase Alta</option>
+                                <option value="2">Clase Baja</option>
                         </select>
-                                
-                                    <div class="errorcantidad red-text"></div>
+                                <div class="errortipo red-text"></div>
                     </div>
-                    <div class="input-field">
-                        <i class="material-icons prefix">attach_money</i>
-                        <input id="costo" type="text" name="costo" data-error=".errorcosto">
-                        <label for="costo">Costo Total</label><br>
-                        <div class="errorcosto red-text"></div>
-                    </div>
-                     </form>
-             </div>
-        
-         <div class="modal-footer">
-                <a href="#" class="btn-flat waves-effects wavs-light btn" id="acepta_registro">Aceptar</a>
-                <a href="#" class="btn-flat waves-effects wavs-light btn  modal-close">Cancelar</a>
-         </div>
-      </div>
-   </div>
-
-   <!-- agregar herramientas o material   -->
-   <div class="modal" id="modmate">
-     <div class="modal-content">
-     <h5 align="center">Material</h5>
-        <br>
-       <div class="container row">
-         <form id="form_material">
-           <div class="input-field">
-                        <i class="material-icons prefix">settings</i>
-                        <input id="material" type="text" name="material" data-error=".errormaterial">
-                        <label for="material">Nombre Del Material</label><br>
-                        <div class="errormaterial red-text"></div>
-              
-                </div>
-                 <div class="input-field">
-                        <i class="material-icons prefix">assignment</i>
-                        <input id="descrimater" type="text" name="descrimater" data-error=".errordescrimater">
-                        <label for="descrimater">Descripcion De La Pieza</label><br>
-                        <div class="errordescrimater red-text"></div>
-                </div>
-                 <div class="input-field">
-                        <i class="material-icons prefix">loupe</i>
-                        <input id="cantidadmate" type="text" name="cantidadmate" data-error=".errorcantidadmate">
-                        <label for="cantidadmate">Cantidad De La Pieza</label><br>
-                        <div class="errorcantidadmate red-text"></div>
-                </div>
-                 <div class="input-field">
-                        <i class="material-icons prefix">brightness_low</i>
-                        <select name="tipom" id="tipom" data-error=".errortipom">
-                                <option value="" disabled selected>Tipo De Pieza</option>
-                                <option value="1">Electrica</option>
-                                <option value="2">Mecanica</option>
-                                <option value="3">Electronica</option>
-                                <option value="4">Fisica</option>
-                        </select>
-                        <div class="errortipom red-text"></div>
-                 </div>
-                 <div class="input-field">
-                        <i class="material-icons prefix">assignment</i>
-                        <input id="descritipom" type="text" name="descritipom" data-error=".errordescritipom">
-                        <label for="descritipom">Descripcion Del Tipo de la Pieza</label><br>
-                        <div class="errordescritipom red-text"></div>
-                </div>
-                <div class="input-field">
-                       <i class="material-icons prefix">attach_money</i>
-                        <input id="costomate" type="text" name="costomate" data-error=".errorcostomate">
-                        <label for="costomate">Costo De La Pieza</label><br>
-                        <div class="errorcostomate red-text"></div>
-                </div>
-         </form>
-       </div>
-     </div>
-     <div class="modal-footer">
-                <a href="#" class="btn-flat waves-effects wavs-light btn" id="acepmaterial">Aceptar</a>
-                <a href="#" class="btn-flat waves-effects wavs-light btn  modal-close">Cancelar</a>
-         </div>
-   </div>
-
-<div class="col l3 offset-l4">
+                   </div> 
+            </form>
+</div>
+       
+  <div class="row">
+        <div class="col s2 offset-l3">
+                <a class="waves-effect waves-light hover_verde btn" id="acepta_registro">Aceptar</a>
+        </div>
+        <div class="col s1">
+                <a href="#" class="waves-effect waves-light hover_rojo btn">Cancelar</a>
+        </div>
+  </div>
+<br>
+<br>
+<br>
+<br>
 <div class="row">
-<div class="col s12 m4 offset-l4">
- <div class="card">
-        <div class="card-image">
-          <img src="../../resources/img/man.jpg">
-          
-        </div>
-        <div class="card-content">
-            <p class="center">Mantenimiento</p>
-        </div>
-      </div>
-    </div>
-    </div> 
-    </div>   
+<table class="col s10 offset-l1">
+            <thead>
+              <tr>
+                  <th data-field="id">No.</th>
+                  <th data-field="name">Nombre</th>
+                  <th data-field="price">No.asientos</th>
+                  <th data-field="name">Descripcion</th>
+                  <th data-field="name">Capacidad</th>
+                  <th data-field="name">Tipo de vuelo</th>
+              </tr>
+            </thead>
 
-    
-</body>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>airplane</td>
+                <td>200</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt magni velit, repudiandae odit soluta unde, deleniti minus illum! Beatae hic at rem, debitis numquam ex nisi maxime quas excepturi facilis.</td>
+                <td>500 litros</td>
+                <td>clase alta</td>
+             </tr>
+              <tr>
+                <td>2</td>
+                <td>airplane</td>
+                <td>300</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores debitis, architecto. Corporis odio a exercitationem eos natus magnam reprehenderit, architecto soluta. Recusandae nemo obcaecati reprehenderit, cumque, tempora exercitationem dolore nostrum.</td>
+                <td>600 litros</td>
+                <td>clase baja</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>airplane</td>
+                <td>500</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid eligendi asperiores rerum aperiam nam esse ratione aliquam quos alias nostrum qui laudantium, nobis dolore tempora iste incidunt quibusdam, quo mollitia.</td>
+                <td>700 litros</td>
+                <td>clase alta</td>
+              </tr>
+              
+            </tbody>
+          </table>
+          </div>
+ </div>
+
+
+  
+</main>
+       
+  </body>
+
 <?php
   include("../partials/footer.php")
 ?>
