15789fa711e53033b726c3e40285accc170eac53
AlfredoNolasco <nolasco_auto24@hotmail.com>
2016-11-11 05:51:22 -0600

    mantenimiento y material acomodamiento

--- views/aviones_mant/index.php
+++ views/aviones_mant/index.php
@@ -68,6 +68,103 @@
          
     
   });
+    $('select').material_select();
+        $("#acepta_registro").click(function(event)
+        {
+            $("#form_mantenimiento").submit();
+        });
+
+        $("#form_mantenimiento").validate({
+        ignore:[],
+        rules:{
+          trabajador:"required",
+          pieza:"required",
+          descripcion:"required",
+          cantidad:"required",
+          costo:
+          {
+            required:true,
+            number:true
+          },
+        },
+        messages:{
+          trabajador:"Ingresa nombre del Mecanico",
+          pieza:"Ingresa el nombre de la pieza",
+          descripcion:"Ingresa una breve descripcion",
+          cantidad:"Selecciona cantidad de pieza",
+          costo:
+          {
+            required:"Ingresa el costo total de reparacion",
+            number:"Ingresa solamente numeros",
+          },
+        },
+
+        errorElement:'div',
+        errorPlacement:function(error,element){
+          var placement=$(element).data("error");
+          if(placement)
+            $(placement).append(error);
+          else
+            error.insertAfter(element);
+        },
+        submitHandler:function(form){
+          alert("todo ok");
+          $("#mantenimiento").closeModal();
+        }
+      });
+        $("#acepmaterial").click(function(event)
+        {
+          $("#form_material").submit();
+        });
+        $("#form_material").validate(
+        {
+          ignore:[],
+          rules:{
+          material:"required",
+          descrimater:"required",
+          cantidadmate:
+          {
+            required:true,
+            number:true
+          },
+          tipom:"required",
+          descritipom:"required",
+          costomate:
+          {
+            required:true,
+            number:true
+          },
+        },
+        messages:{
+          material:"Ingresa nombre del Mecanico",
+          descrimater:"Ingresa el nombre de la pieza",
+          cantidadmate:
+          {
+            required:"Ingresa una breve descripcion",
+            number:"Solamente Numeros"
+          },
+          tipom:"Selecciona cantidad de pieza",
+          descritipom:"Ingresa una breve descripcion del tipo de la pieza",
+          costomate:
+          {
+            required:"Ingresa el costo de la pieza",
+            number:"Ingresa solamente numeros"
+          },
+        },
+
+        errorElement:'div',
+        errorPlacement:function(error,element){
+          var placement=$(element).data("error");
+          if(placement)
+            $(placement).append(error);
+          else
+            error.insertAfter(element);
+        },
+        submitHandler:function(form){
+          // alert("todo ok");
+          // $("#mantenimiento").closeModal();
+        }
+        });
   });
  </script>
 
@@ -77,6 +174,7 @@
             <ul class="tabs center">
               <li class="tab col l3 m4 s12" id="id_aviones" class="id_aviones" ><a class="indigo-text" href="#test1" >AVIONES</a></li>
               <li class="tab col l3 m4 s12"><a class="indigo-text" id="id_mantenimiento" class="id_mantenimiento" href="#test2">MANTENIMIENTO</a></li>
+              <li class="tab col l3 m4 s12"><a class="indigo-text" id="id_material" class="id_material" href="#test4">MATERIAL</a></li>
               <li class="tab col l3 m4 s12"><a class="indigo-text" id="id_informe" class="id_informe" href="#test3">INFORME</a></li>
             </ul>
        </div>
@@ -200,8 +298,199 @@
           </table>
           </div>
  </div>
+<div id="test2">
+  <div class="row">
+        <div class="col l7 m6 s12">
+        <h5 align="center">Mantenimiento</h5>
+        <br>
+          <div class="container row">
+                <form id="form_mantenimiento" name="form_mantenimiento">
+                <div class="input-field">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="trabajador" type="text" name="trabajador" data-error=".errortrabajador">
+                        <label for="trabajador">Nombre Del Mecanico</label><br>
+                        <div class="errortrabajador red-text"></div>
+              
+                </div>
+                 <div class="input-field">
+                        <i class="material-icons prefix">settings</i>
+                        <input id="pieza" type="text" name="pieza" data-error=".errorpieza">
+                        <label for="pieza">Nombre De La Pieza</label><br>
+                        <div class="errorpieza red-text"></div>
+                </div>
+                <div class="input-field">
+                        <i class="material-icons prefix">assignment</i>
+                        <textarea id="descripcion" name="descripcion" class="validate" data-error=".errordescripcion" class="materialize-textarea" rows="10" placeholder="Breve Descripcion De La Pieza"></textarea>
+                        <label for="descripcion"><b>Descripcion De La Pieza</b></label>
+                        <div class="errordescripcion red-text"></div>
+              
+              
+                    </div>
+                <div class="input-field">
+                        <i class="material-icons prefix">loupe</i>
+                        <select name="cantidad" id="cantidad" data-error=".errorcantidad">
+                                <option value="" disabled selected>Cantidad De Piezas</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="6">7</option>
+                                <option value="6">8</option>
+                                <option value="6">9</option>
 
+                        </select>
+                                
+                                    <div class="errorcantidad red-text"></div>
+                    </div>
+                    <div class="input-field">
+                        <i class="material-icons prefix">attach_money</i>
+                        <input id="costo" type="text" name="costo" data-error=".errorcosto">
+                        <label for="costo">Costo Total</label><br>
+                        <div class="errorcosto red-text"></div>
+                    </div>
+                     <div class="col l6">
+                    <a class="waves-effect waves-light hover_rojo btn right">Cancelar</a>
+                  </div>
+                    <div class="col l6">
+                      <a class="waves-effect waves-light hover_verde darken-3 btn  right" id="acepta_registro">Aceptar</a>
+                    </div>
+                     </form>
+             </div>
+        </div> 
+          <div class="col l5 m6 s12">
+          <h5 align="center">Mantenimiento Registrado</h5><br>
+      <table class="responsive-table">
+        <thead>
+          <tr>
+              <th data-field="id">No.</th>
+              <th data-field="name">Nombre</th>
+              <th data-field="price">Cantidad</th>
+              <th data-field="price">Costo</th>
 
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>11</td>
+            <td>Eclair</td>
+            <td>2</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>22</td>
+            <td>Jellybean</td>
+            <td>3</td>
+            <td>$3.76</td>
+          </tr>
+          <tr>
+            <td>333</td>
+            <td>Lollipop</td>
+            <td>9</td>
+            <td>$7.00</td>
+          </tr>
+        </tbody>
+        <!-- <a href="#!" class="secondary-content"><i class="material-icons">send</i></a> -->
+      </table>
+     </div>
+  </div>
+</div>
+<div id="test4">
+  <div class="row">
+      <div class="col l7 m6 s12">
+       <h5 align="center">Material</h5>
+        <br>
+       <div class="container row">
+         <form id="form_material">
+           <div class="input-field">
+                        <i class="material-icons prefix">settings</i>
+                        <input id="material" type="text" name="material" data-error=".errormaterial">
+                        <label for="material">Nombre Del Material</label><br>
+                        <div class="errormaterial red-text"></div>
+              
+                </div>
+                 <div class="input-field">
+                        <i class="material-icons prefix">assignment</i>
+                        <input id="descrimater" type="text" name="descrimater" data-error=".errordescrimater">
+                        <label for="descrimater">Descripcion De La Pieza</label><br>
+                        <div class="errordescrimater red-text"></div>
+                </div>
+                 <div class="input-field">
+                        <i class="material-icons prefix">loupe</i>
+                        <input id="cantidadmate" type="text" name="cantidadmate" data-error=".errorcantidadmate">
+                        <label for="cantidadmate">Cantidad De La Pieza</label><br>
+                        <div class="errorcantidadmate red-text"></div>
+                </div>
+                 <div class="input-field">
+                        <i class="material-icons prefix">brightness_low</i>
+                        <select name="tipom" id="tipom" data-error=".errortipom">
+                                <option value="" disabled selected>Tipo De Pieza</option>
+                                <option value="1">Electrica</option>
+                                <option value="2">Mecanica</option>
+                                <option value="3">Electronica</option>
+                                <option value="4">Fisica</option>
+                        </select>
+                        <div class="errortipom red-text"></div>
+                 </div>
+                 <div class="input-field">
+                        <i class="material-icons prefix">assignment</i>
+                        <input id="descritipom" type="text" name="descritipom" data-error=".errordescritipom">
+                        <label for="descritipom">Descripcion Del Tipo de la Pieza</label><br>
+                        <div class="errordescritipom red-text"></div>
+                </div>
+                <div class="input-field">
+                       <i class="material-icons prefix">attach_money</i>
+                        <input id="costomate" type="text" name="costomate" data-error=".errorcostomate">
+                        <label for="costomate">Costo De La Pieza</label><br>
+                        <div class="errorcostomate red-text"></div>
+                </div>
+                <div class="col l6">
+                    <a class="waves-effect waves-light btn hover_rojo right">Cancelar</a>
+                  </div>
+                 <div class="col l6">
+                   <a class="waves-effect waves-light btn hover_verde darken-3 right" id="acepmaterial">Aceptar</a>
+                 </div>
+         </form>
+       </div>
+     </div>
+     <div class="col l5 m6 s12">
+     <h5 align="center">Material Almacenado</h5><br>
+      <table class="responsive-table">
+        <thead>
+          <tr>
+              <th data-field="id">No.</th>
+              <th data-field="name">Nombre</th>
+              <th data-field="price">Cantidad</th>
+              <th data-field="price">Costo</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>11</td>
+            <td>Eclair</td>
+            <td>2</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>22</td>
+            <td>Jellybean</td>
+            <td>3</td>
+            <td>$3.76</td>
+          </tr>
+          <tr>
+            <td>333</td>
+            <td>Lollipop</td>
+            <td>9</td>
+            <td>$7.00</td>
+          </tr>
+        </tbody>
+      </table>
+     </div>
+     </div>
+</div>
   
 </main>
        
