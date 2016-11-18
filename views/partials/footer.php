ed091326249a9daa3a774ec2f8ec9063030ecb09
elirobles <elitesvb@gmail.com>
2016-11-17 21:23:29 -0600

    modificacion footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,9 +1,36 @@
   <footer> 
         <div class="footer-copyright indigo">
           <div class="container center white-text">
-              &copy 2016 Aeroline702
-              | Acerca del Sitio | Avisos Legales | Mapa de Sitio | Contáctanos |
+              <div class="row">
+              	<div class="col l11 m11 s11 titulo_footer">
+             	 &copy 2016 Aeroline702
+             	</div>
+              	<div class="col l1 m1 s1 texto_footer white-text">
+               		Contáctanos
+
+                </div>
+              </div>
           </div>
+          <div class="center white-text">
+              <div class="row">
+              	<div class="col l2 m2 s2">
+              		<a href="#" class="texto_footer white-text">|    Ayuda |</a>
+              	</div>
+              	<div class="col l2 m2 s2">
+              		<a href="#" class="texto_footer white-text">|    Acerca de nosotros |</a>
+              	</div>
+			  	<div class="col l2 m2 s2">
+              		<a href="#" class="texto_footer white-text">|    Informacion legal |</a>
+               	</div> 
+               	<div class="col l2 m2 s2">
+              		<a href="#" class="texto_footer white-text">|    condiciones de transporte |</a>
+               	</div>
+               	<div class="col l4 m4 s4">
+              		<a href="http://www.facebook.com/" target="_blank"><img src="../../resources/img/fbk.png" class="iconito" />Facebook</a>
+              		<a href="http://twitter.com/" target="_blank"><img src="../../resources/img/tw.png" class="iconito" />Twitter</a>
+					<a href="http://twitter.com/" target="_blank"><img src="../../resources/img/wat.png" class="iconito" />WhatsApp</a>
+              	</div>
+              </div>
           
         </div>  
   </footer>
